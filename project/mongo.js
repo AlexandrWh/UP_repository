039312f365d9f9f92fa25d82fa671f9ed8d405ac
Alexander <alexandrkuzmich@mail.ru>
2018-05-27 21:31:03 +0200

    add some method's to mongo

--- project/mongo.js
+++ project/mongo.js
@@ -1,33 +1,63 @@
 const mongoose = require('mongoose');
 const PhotoPostSchema = new mongoose.Schema({
-    id: {type:String},
-    description: {type:String},
-    createdAt: {type:Date},
-    author: {type:String},
-    photoLink: {type:String},
-    likes: {type:Array},
-    tags: {type:String}
+    id: String,
+    description: String,
+    createdAt: Date,
+    author: String,
+    photoLink: String,
+    likes: Array,
+    tags: String
 });
 
 const PhotoPost = mongoose.model('photoposts',PhotoPostSchema);
 
-module.exports = {
-    addPhotoPost
-};
-
 function addPhotoPost(post) {
     return PhotoPost.findOne({id: post.id}).then(photopost=>{
         if(!photopost){
-        photopost = new PhotoPost(post);
-        return photopost.save();
-    }
-})
+            photopost = new PhotoPost(post);
+            return photopost.save();
+    }});
 }
 
-/*function editPhotoPost(id, content) {
-
+function getPhotoPost(id){
+    return PhotoPost.findOne({id: id}).then((post) => {
+        console.log(post);
+        return post;
+    });
 }
 
 function deletePhotoPost(id) {
+    return PhotoPost.findOneAndDelete({id: id}).then((post)=>{
+        console.log(post);
+        return post;
+    });
+}
 
-}*/
+function editPhotoPost(id, content) {
+    return PhotoPost.findOneAndUpdate({id: id}, { $set: content}).then((post) =>{
+        console.log(post);
+        return post;
+    })
+}
+
+function likePhotoPost(id, user) {
+    return PhotoPost.findOne({id: id}).then(post=>{
+        post.likes.push(user);
+        post.save();
+        console.log(post);
+    });
+}
+
+module.exports = {
+    addPhotoPost,
+    getPhotoPost,
+    editPhotoPost,
+    deletePhotoPost,
+    likePhotoPost
+};
+
+
+
+
+
+
